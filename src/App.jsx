1efd81a4f603a4d01a45fd4adc9e33e5afde1027
yugankfatehpuria4 <yugankfatehpuria786@gmail.com>
2025-04-30 23:55:31 +0530

    Updated file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
 
   const handleLogin = (email, password) => {
-    if (email == 'admin@me.com' && password == '123') {
+    if (email == 'admin@me.com' && password == '123456') {
       setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
     } else if (userData) {
